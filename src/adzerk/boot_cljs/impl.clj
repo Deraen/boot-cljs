646b46e9cb4467e8a75981eaf7f909f86bcfc413
Juho Teperi <juho.teperi@iki.fi>
2015-03-30 18:48:03 +0300

    Storing compiler env is required for fast rebuilds

--- src/adzerk/boot_cljs/impl.clj
+++ src/adzerk/boot_cljs/impl.clj
@@ -1,12 +1,20 @@
 (ns adzerk.boot-cljs.impl
   (:require [clojure.java.io :as io]
-            [cljs.closure    :as cljs]))
+            [cljs.closure    :as cljs]
+            [cljs.env        :as env]))
 
 (defrecord CljsSourcePaths [paths]
   cljs/Compilable
   (-compile [this opts]
     (mapcat #(cljs/-compile % opts) paths)))
 
+(def ^:private stored-env (atom nil))
+
+(defn cljs-env [opts]
+  (compare-and-set! stored-env nil (env/default-compiler-env opts))
+  @stored-env)
+
 (defn compile-cljs
   [src-paths opts]
-  (cljs/build (CljsSourcePaths. (filter #(.exists (io/file %)) src-paths)) opts))
+  (binding [env/*compiler* (cljs-env opts)]
+    (cljs/build (CljsSourcePaths. (filter #(.exists (io/file %)) src-paths)) opts)))
